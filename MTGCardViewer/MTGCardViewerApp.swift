d54077c1e8c62756b22db5debcc8cbd45128a4ed
Michael Nguyen <techdragon.nguyen@gmail.com>
2024-03-04 19:11:13 -0800

    Initial Commit

---
+++ MTGCardViewer/MTGCardViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTGCardViewerApp.swift
+//  MTGCardViewer
+//
+//  Created by Michael Nguyen on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MTGCardViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
